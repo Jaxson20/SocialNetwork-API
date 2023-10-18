50d27f9ecdcb8dc93f54bcb074376aec6ad0a004
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-10-18 13:08:48 -0500

    models??

---
+++ models/reaction.js
@@ -0,0 +1,25 @@
+const { Schema, Types } = require('mongoose'); 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Types.ObjectId, 
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 2,
+        default: 'no text entered',
+    },
+    username: {
+        type: String,
+        required: true,
+        default: 'default user',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = reactionSchema; 
